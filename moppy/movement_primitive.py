39be2e32260d4c0ccfd5ccc8b23a4dabc871296c
admin <dup2_x2@protonmail.com>
2024-06-03 15:05:55 +0200

    movement primitive interface

--- moppy/movement_primitive.py
+++ moppy/movement_primitive.py
@@ -1,5 +1,12 @@
 class MovementPrimitive:
-
+    """
+    MPs are parametric models that
+    capture the essential features of a motion while allowing
+    for variations and modifications according to different situations.
     """
 
-    """
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"MP: {self.name}"
