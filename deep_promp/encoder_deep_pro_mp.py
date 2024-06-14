b3e81df3c2f9862f58104c41a7328aef95e285cc
admin <dup2_x2@protonmail.com>
2024-06-14 12:32:14 +0200

    name the stddev sigma instead of rho, cleaner bayesian aggregation code (untested)

--- deep_promp/encoder_deep_pro_mp.py
+++ deep_promp/encoder_deep_pro_mp.py
@@ -7,6 +7,25 @@ from interfaces.latent_encoder import LatentEncoder
 from trajectory.trajectory import Trajectory
 
 
+def bayesian_aggregation(mu_sigma_points: List[Tuple[np.ndarray, np.ndarray]]) -> Tuple[np.ndarray, np.ndarray]:
+    mu_as, sigma_as = zip(*mu_sigma_points)
+
+    # TODO verify if the sum is calculated correctly
+    sum_mu_a_over_sigma_a_squared = np.sum(mu_as / sigma_as ** 2, axis=0)
+    sum_sigma_a_inverse = np.sum(1 / sigma_as ** 2, axis=0)
+
+    # Calculate sigma_z^2(A) without context variables
+    sigma_z_sq = 1 / (1 + sum_sigma_a_inverse)
+
+    # Adjust for context variables if needed
+    # This part is omitted since the assumption is about having only via-points (A)
+
+    # Calculate mu_z(A) using the formula without context variables
+    mu_z = sigma_z_sq * sum_mu_a_over_sigma_a_squared
+
+    return mu_z, sigma_z_sq
+
+
 class EncoderDeepProMP(LatentEncoder):
 
     def __init__(self,
@@ -45,57 +64,32 @@ class EncoderDeepProMP(LatentEncoder):
         # (including bayesian aggregation).
 
         # 1. For each traj_point tuple (t_i, x_i) in traj_points,
-        # pass it through the encoder network and get the vectors mu_i and rho_i.
+        # pass it through the encoder network and get the vectors mu_i and sigma_i.
 
-        points_mu_rho = []
+        points_mu_sigma = []
 
         for t, x in traj_points:
-            # mu_i and rho_i are vectors with the same dimension as the TrajectoryState that is being used.
+            # mu_i and sigma_i are vectors with the same dimension as the TrajectoryState that is being used.
             output = self.net(x.to_vector())
             # Check if the output shape is 2 * trajectory_state_dimensions
             if not output.shape[0] == 2 * x.get_dimensions():
                 raise ValueError(
-                    "The output shape of the encoder network should have a mu and rho for each dimension of the "
+                    "The output shape of the encoder network should have a mu and sigma for each dimension of the "
                     "TrajectoryState.")
 
-            mu_point, rho_point = output[:x.get_dimensions()], output[x.get_dimensions():]
-            points_mu_rho.append((mu_point, rho_point))
+            mu_point, sigma_point = output[:x.get_dimensions()], output[x.get_dimensions():]
+            points_mu_sigma.append((mu_point, sigma_point))
 
         # TODO get hyperparamenter for dimension of latent variable
 
-        # 2. Calculate the vectors mu_z and rho_z using the formulas on top right of page 3.
-        mu_z, rho_z = self.bayesian_aggregation(points_mu_rho)
+        # 2. Calculate the vectors mu_z and sigma_z using the formulas on top right of page 3.
+        mu_z, sigma_z_sq = bayesian_aggregation(points_mu_sigma)
 
         # 3. Sample the latent variable z vector.
-        # This is done by sampling each element of z from a normal distribution specified by mu_z and rho_z.
-        z_sampled = np.random.normal(mu_z, rho_z)
+        # This is done by sampling each element of z from a normal distribution specified by mu_z and sigma_z.
+        z_sampled = np.random.normal(mu_z, sigma_z_sq)  # TODO use sqrt of sigma_z_sq or not?
 
         return z_sampled
-
-    def bayesian_aggregation(self, mu_rho_points: List[Tuple[np.ndarray, np.ndarray]]) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Formula with both via-points(A) and context variables(C)
-        \mu_z(A,C)=\sigma_z^2(A,C)\left(\sum_{i=1}^{n}\frac{\mu_a(a_i)}{\sigma_a^2(a_i)}+\sum_{i=1}^{m}\frac{\mu_c(c_i)}{\sigma_c^2(c_i)}\right)
-        \sigma_z^2(A,C)=\left(1+\sum_{i=1}^{n}\sigma_a^2(a_i)^{-1}+\sum_{i=1}^{m}\sigma_c^2(c_i)^{-1}\right)^{-1}
-        
-        Assumtion: We only have via-points(A) and no context variables(C) | mu_i == \mu_a(a_i) and rho_i == \sigma_a^2(a_i)
-        Formula without context variables(C):
-        \mu_z(A)=\sigma_z^2(A)\left(\sum_{i=1}^{n}\frac{\mu_a(a_i)}{\sigma_a^2(a_i)}\right)
-        \sigma_z^2(A)=\left(1+\sum_{i=1}^{n}\sigma_a^2(a_i)^{-1}\right)^{-1}
-        """
-
-        # Calculate the summations in the formulas.
-        rho_sum = 0
-        mu_div_roh_sum = 0
-        for mu, rho in mu_rho_points:
-            roh_sum += 1 / rho
-            mu_div_roh_sum += mu / rho
-
-        # Calculate the final mu_z and sigma_z according to the assumptions.
-        sigma_z = 1 / (1 + roh_sum)
-        mu_z = sigma_z * mu_div_roh_sum
-
-        return (mu_z, sigma_z)
 
     def __str__(self):
         ret: str = "EncoderDeepProMP(neurons=%s)" % self.neurons
