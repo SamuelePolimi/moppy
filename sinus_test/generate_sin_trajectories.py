3a77505dd0097eccaf8fc563c3a64f65943baeca
paulpruenster <paul.pruenster@gmail.com>
2024-07-04 11:04:06 +0200

    sinus trajectories

--- sinus_test/generate_sin_trajectories.py
+++ sinus_test/generate_sin_trajectories.py
@@ -1,1 +1,22 @@
 
+import torch
+import random
+import numpy
+
+traj = []
+num_steps = 100
+time = 0.0
+
+amplitude = random.randint(1, 10)
+frequency = random.randint(1, 5)
+
+for i in range(10):
+    for _ in range(num_steps):
+        vec = {
+                'value': amplitude * numpy.sin(frequency * time),
+                'time': time
+            }
+        traj.append(vec)
+        time += 1/num_steps
+
+    torch.save(traj, f"trajectories/sin_{i}.pth")
