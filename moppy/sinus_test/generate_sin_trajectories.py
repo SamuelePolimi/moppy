06bf459e913c5607551cbd6a8d8a1c2e26836ba6
paulpruenster <paul.pruenster@gmail.com>
2024-07-04 12:40:15 +0200

    new trajectories

--- moppy/sinus_test/generate_sin_trajectories.py
+++ moppy/sinus_test/generate_sin_trajectories.py
@@ -4,21 +4,20 @@ import random
 import numpy
 
 traj = []
-num_steps = 100
-time = 0.0
+num_steps = 50
 
 amplitude = random.randint(1, 10)
 frequency = random.randint(1, 5)
 
 for i in range(50):
+    time = 0.0
     for _ in range(num_steps + 1):
+        sin_val = amplitude * numpy.sin(frequency * time * 2 * numpy.pi)
         vec = {
-                'value': amplitude * numpy.sin(frequency * time),
+                'value': sin_val.item(),
                 'time': time
             }
         traj.append(vec)
         time += 1/num_steps
-
-    traj[-1]["time"] = 1.0
 
     torch.save(traj, f"trajectories/sin_{i}.pth")
