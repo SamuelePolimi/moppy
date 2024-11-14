37be79cfe2a28a9545f93abf80925ca1dbd51b07
Matthias Ebn <matthiasebner491@gmail.com>
2024-11-14 10:15:38 +0100

    update kl_plotting test

--- moppy/deep_promp/test/plot_kl_annealing.py
+++ moppy/deep_promp/test/plot_kl_annealing.py
@@ -2,8 +2,17 @@ from moppy.deep_promp.deep_promp import DeepProMP
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(1, 1000, 1000)
+n_cycles_values = [4, 50, 100, 500, 1000]  # Array of different n_cycles values
+i = 1000  # Fixed i value
 
-y = np.array([DeepProMP.kl_annealing_scheduler(t) for t in x])
-plt.plot(x, y)
+fig, axs = plt.subplots(len(n_cycles_values), 1, figsize=(10, 15))
+
+for ax, n_cycles in zip(axs, n_cycles_values):
+    x = np.linspace(1, i, i)
+    y = np.array([DeepProMP.kl_annealing_scheduler(t, n_cycles=n_cycles, max_epoch=i) for t in x])
+    ax.plot(x, y, label=f'n_cycles={n_cycles}')
+    ax.legend()
+
+fig.suptitle(f'KL Annealing Schedules (max_epochs={i})', fontsize=16)
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 plt.show()
