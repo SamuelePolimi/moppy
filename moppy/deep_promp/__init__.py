b3cda718fb707513e601efd43866100deac8771f
Matthias Ebn <matthiasebner491@gmail.com>
2024-12-18 14:45:31 +0100

    add TrainEncoderAsActor to innit

--- moppy/deep_promp/__init__.py
+++ moppy/deep_promp/__init__.py
@@ -2,6 +2,7 @@ from .encoder_deep_pro_mp import EncoderDeepProMP
 from .decoder_deep_pro_mp import DecoderDeepProMP
 from .deep_promp import DeepProMP
 from. deep_promp_lighning_trainer import DeepProMo_Lighning
+from .train_encoder_as_actor import TrainEncoderAsActor
 from .encoder_as_actor import EncoderAsActor, RobosuiteDemoStartingPosition
 
 from .utils import (
