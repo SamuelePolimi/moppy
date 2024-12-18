c9d3efb1a77097bc23fc093a5b7b9f251d0e4f8f
Matthias Ebn <matthiasebner491@gmail.com>
2024-12-18 13:31:13 +0100

    add RobosuiteDemoStartingPosition to __init__

--- moppy/deep_promp/__init__.py
+++ moppy/deep_promp/__init__.py
@@ -2,7 +2,7 @@ from .encoder_deep_pro_mp import EncoderDeepProMP
 from .decoder_deep_pro_mp import DecoderDeepProMP
 from .deep_promp import DeepProMP
 from. deep_promp_lighning_trainer import DeepProMo_Lighning
-from .encoder_as_actor import EncoderAsActor
+from .encoder_as_actor import EncoderAsActor, RobosuiteDemoStartingPosition
 
 from .utils import (
     set_seed, plot_trajectories, generate_sin_trajectory, generate_sin_trajectory_set,
